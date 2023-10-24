4f1349f591a0d5c83d59a48aec69ba1faee74b2a
will lutz <wlutz@bsd220.org>
2023-10-24 09:31:16 -0500

    Initial Commit

---
+++ intergalactic travel/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  intergalactic travel
+//
+//  Created by will lutz on 10/9/23.
+//
+
+import SwiftUI
+struct ContentView: View {
+    var body: some View {
+        NavigationView{
+            VStack{
+                StarLink(name: "blue star")
+                .padding()
+                StarLink(name: "red star")
+                .padding()
+                }
+                .foregroundColor(.red)
+            }
+            .navigationBarTitle("Intergalactic Travel", displayMode: .inline)
+        }
+    }
+
+struct ContentView_Previews: PreviewProvider {
+            static var previews: some View {
+                ContentView()
+            }
+        }
+struct StarLink: View {
+    let name: String
+    var body: some View {
+        NavigationLink("Travel to \(name)") {
+            Image (name)
+                .resizable ()
+                .aspectRatio(contentMode: .fill)
+        }
+    }
+}
---
+++ intergalactic travel/intergalactic_travelApp.swift
@@ -0,0 +1,17 @@
+//
+//  intergalactic_travelApp.swift
+//  intergalactic travel
+//
+//  Created by will lutz on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct intergalactic_travelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
